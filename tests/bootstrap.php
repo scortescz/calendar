724430b100fb54fad21471e54c867f0489028e33
Zdeněk Drahoš (zdenekdrahos) <drahoszdenek@gmail.com>
2015-08-23 11:01:05 +0200

    Delete stm integration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,3 @@ require_once(__DIR__ . '/../autoload.php');
 
 require_once(__DIR__ . '/STM/Plugin/Calendar/CalendarAsserts.php');
 require_once(__DIR__ . '/STM/Plugin/Calendar/Events/EventsIteratorUtil.php');
-require_once(__DIR__ . '/STM/Plugin/Calendar/StmIntegration/Matches/MatchesMock.php');
-require_once(__DIR__ . '/STM/Plugin/Calendar/StmIntegration/Matches/MatchesLoaderMock.php');
-
-define('STM_BOOTSTRAP', __DIR__ . '/../../_sports_table_manager/stm/bootstrap.php');
