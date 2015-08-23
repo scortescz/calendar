ec2c39fcc6f5ff2a6be9878d4cdb4ff9242fceca
Zdeněk Drahoš (zdenekdrahos) <drahoszdenek@gmail.com>
2015-08-23 18:35:28 +0200

    Refactoring - calendar - refine example

--- example/index.php
+++ example/index.php
@@ -1,23 +1,18 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$currentYear = date('Y');
-$currentMonth = date('n');
-$previousMonth = $currentMonth - 1;
-$nextMonth = $currentMonth + 1;
-
 $request = new \Scortes\Calendar\CalendarRequest();
 $request->dateStart = new DateTime('now - 2 month');
 $request->dateEnd = null; // use max date from events
 $request->events = array(
-    "{$currentYear}-{$previousMonth}-10" => 'Day in previous month',
-    "{$currentYear}-{$currentMonth}-1" => 'First day in month',
-    "{$currentYear}-{$currentMonth}-16" => '16th day in month',
-    "{$currentYear}-{$nextMonth}-17" => 'Day in next month',
+    "now - 1 month" => 'Day in previous month',
+    date('Y-n-') . 1 => 'First day in month',
+    date('Y-n-') . 16 => '16th day in month',
+    "now + 1 month" => 'Day in next month',
 );
 $request->addEvent(new DateTime('now + 2 months'), 'now + 2 months');
 
-$response = Scortes\Calendar\createCalendar($request);
+$calendar = Scortes\Calendar\createCalendar($request);
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,17 +29,17 @@ $response = Scortes\Calendar\createCalendar($request);
         <ul>
             <li>Current date: 
                 <a href="#today"><strong>
-                    <?php echo $response->today->date->format('j.n.Y'); ?>
+                    <?php echo $calendar->today->date->format('j.n.Y'); ?>
                 </strong></a>
             </li>
-            <li>Current month: <a href="#currentMonth"><strong><?php echo $response->today->monthNumber; ?></strong></a></li>
-            <li>Current week: <a href="#currentWeek"><strong><?php echo $response->today->weekNumber; ?></strong></a></li>
+            <li>Current month: <a href="#currentMonth"><strong><?php echo $calendar->today->monthNumber; ?></strong></a></li>
+            <li>Current week: <a href="#currentWeek"><strong><?php echo $calendar->today->weekNumber; ?></strong></a></li>
         </ul>
         
         <h2>Basic calendar</h2>
         <?php
-        foreach ($response->months as $month) {
-            $isCurrentMonth = $response->today->isCurrentMonth($month);
+        foreach ($calendar->months as $month) {
+            $isCurrentMonth = $calendar->today->isCurrentMonth($month);
             $monthId = $isCurrentMonth ? ' id="currentMonth"' : '';
             echo "<h3{$monthId}>Month {$month->monthNumber}/{$month->year}</h3>";
 
@@ -52,16 +47,16 @@ $response = Scortes\Calendar\createCalendar($request);
             $weekDay = 1;
             echo '<table>';
             for ($week = 0; $week < $month->weeksCount; $week++) {
-                $isCurrentWeek = $isCurrentMonth && $response->today->isCurrentWeek($month, $week);
+                $isCurrentWeek = $isCurrentMonth && $calendar->today->isCurrentWeek($month, $week);
                 $weekId = $isCurrentWeek ? ' id="currentWeek"' : '';
                 echo "<tr{$weekId}>";
                 for ($day = 0; $day < 7; $day++) {
-                    list($isDayInMonth, $isCurrentDay) = $response->today->isCurrentDay($month, $weekDay++, $currentDay);
+                    list($isDayInMonth, $isCurrentDay) = $calendar->today->isCurrentDay($month, $weekDay++, $currentDay);
                     if ($isDayInMonth) {
                         $weekId = $isCurrentWeek && $isCurrentDay ? ' id="today"' : '';
                         echo "<td{$weekId}>";
                         $eventKey = "{$month->year}-{$month->monthNumber}-{$currentDay}";
-                        $event = $response->events->find($eventKey);
+                        $event = $calendar->events->find($eventKey);
                         if ($event) {
                             echo "<strong title='{$event}'>" . $currentDay . '</strong>';
                         } else {
@@ -79,18 +74,22 @@ $response = Scortes\Calendar\createCalendar($request);
         }
         ?>
 
+
         <h2>Timeline of events</h2>
+        <h3>Input event dates</h3>
+        <pre><?php echo implode(', ', array_keys($request->events)); ?></pre>
+
         <h3>All events</h3>
         <?php printEventsList(''); ?>
         
         <h3>Events in current month</h3>
-        <?php printEventsList("{$response->today->year}-{$response->today->monthNumber}"); ?>
+        <?php printEventsList("{$calendar->today->year}-{$calendar->today->monthNumber}"); ?>
 
         <?php
         function printEventsList($key) {
-            global $response;
+            global $calendar;
             echo '<ul>';
-            foreach ($response->events->iterate($key) as $event) {
+            foreach ($calendar->events->iterate($key) as $event) {
                 echo "<li>{$event->date} - <strong>{$event->unwrap()}</strong></li>";
             }
             echo '</ul>';
